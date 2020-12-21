3b10cb5c1ec7f5454ec8af387dc5be42590d8c37
Alexander Trifonov <mobious96@gmail.com>
2020-12-21 17:54:56 +0300

    Delete FileGenerator.h

--- FileGenerator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-//* int lenmin - min lenght of a generated string
-//* int lenmax - max length of a generated string
-//* int amount - amount of generated strings
-//* filename = "input.txt" - file name where output will be generated
-void GenerateFile(unsigned short int lenmin = 3, unsigned short int lenmax = 7, unsigned int amount = 1000, std::string filename = "input.txt");
